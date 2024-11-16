6708c952233a1743d2637b1a2b71ee48358a41ee
gitzak <zakaria.belrhali@gmail.com>
2024-11-16 17:02:41 +0100

    first commit

---
+++ App.jsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from './src/screen/HomeScreen';
+import ProductDetailsScreen from './src/screen/ProductDetailsScreen';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name={'PRODUCT_DETAILS'}
+          component={ProductDetailsScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
