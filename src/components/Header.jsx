2b071a2e13745a8941c30f79b24f9b863153f0b8
GitZak <zakaria.belrhali@gmail.com>
2024-11-16 18:30:49 +0100

    modif colors

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import {iconSize} from '../constants/dimensions';
+import {colors} from '../constants/colors';
 
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import {useNavigation} from '@react-navigation/native';
@@ -15,10 +16,10 @@ const Header = () => {
         onPress={() => {
           navigaiton.goBack();
         }}>
-        <Ionicons name={'arrow-back'} size={iconSize.md} />
+        <Ionicons name={'arrow-back'} size={iconSize.md} color={colors.gray} />
       </TouchableOpacity>
       <TouchableOpacity>
-        <AntDesign name={'hearto'} size={iconSize.md} />
+        <AntDesign name={'hearto'} size={iconSize.md} color={colors.gray} />
       </TouchableOpacity>
     </View>
   );
